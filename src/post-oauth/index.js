3dc881208fc6ce5fff7d26eeff9a33767d6f4f6f
Nikita <nikita.khromov@onlyoffice.com>
2023-03-22 12:04:28 +0300

    Added service for OAuth zoom authorization. Changed sign service for work with static credentials.

---
+++ src/post-oauth/index.js
@@ -0,0 +1,56 @@
+const AWSXRay = require('aws-xray-sdk-core')
+const AWS = AWSXRay.captureAWS(require('aws-sdk'))
+const needle = require('needle');
+
+// app credentials for OAuth zoom app.
+const client_id = "";
+const secret_id = "";
+
+// Handler
+exports.handler = async function(event, context) {
+    if (!event["body"]) {
+		return "Invalid body";
+	}
+	
+	function getPromise(event) {
+		return new Promise(function(resolve) {
+			try {
+				let oParsedData	= JSON.parse(event["body"]);
+				let method		= "POST";
+				let targetUrl	= "https://zoom.us/oauth/token";
+				let data		= {
+					"code": oParsedData["code"],
+					"grant_type": "authorization_code",
+					"redirect_uri": oParsedData["redirect_uri"]
+				};
+				let options = {
+					headers: {
+						"Host": "zoom.us",
+						"Authorization": 'Basic ' + Buffer.from(`${client_id}:${secret_id}`).toString('base64'),
+						"Content-Type": "application/x-www-form-urlencoded"
+					}
+				};
+				
+				if (!targetUrl || !method || !options["headers"]["Authorization"] || !data["code"]) {
+					resolve("Error of request");
+				}
+				needle(method, targetUrl, data, options)
+					.then((target_response) => {
+						console.log(`Status: ${target_response.statusCode}`);
+						console.log('Body: ', target_response.body);
+						resolve(JSON.stringify(target_response.body));
+					}).catch((err) => {
+						console.log(err);
+						resolve(err.message);
+				});
+			}
+			catch (err) {
+				console.log(err);
+				resolve(err.message);
+			}
+		});
+	}
+	
+	let result = await getPromise(event);
+	return result;
+}
--- src/post-sign/index.js
+++ src/post-sign/index.js
@@ -1,11 +1,15 @@
-const AWSXRay = require('aws-xray-sdk-core')
-const AWS = AWSXRay.captureAWS(require('aws-sdk'))
-const needle = require('needle');
-const KJUR = require('jsrsasign');
+const AWSXRay	= require('aws-xray-sdk-core')
+const AWS		= AWSXRay.captureAWS(require('aws-sdk'))
+const needle	= require('needle');
+const KJUR		= require('jsrsasign');
+
+// app credentials for sdk meetings zoom app.
+const client_id = "";
+const secret_id = "";
 
 // Handler
 exports.handler = async function(event, context) {
-	let SDK_KEY, SDK_SECRET, meet_number, role_id;
+	let meet_number, role_id;
 	if (!event["body"]) {
 		return "Invalid body";
 	}
@@ -17,13 +21,11 @@ exports.handler = async function(event, context) {
 	catch (err) {
 		return "Invalid body";
 	}
-	SDK_KEY = data["sdk_key"];
-	SDK_SECRET = data["sdk_secret"];
-	meet_number = data["meet_number"];
-	role_id = data["role_id"];
+	meet_number	= data["meet_number"];
+	role_id		= data["role_id"];
 
-	if (SDK_KEY && SDK_SECRET && meet_number && role_id != undefined) {
-		return generateSignature(SDK_KEY, SDK_SECRET, meet_number, role_id);
+	if (client_id && secret_id && meet_number && role_id != undefined) {
+		return generateSignature(client_id, secret_id, meet_number, role_id);
 	}
 	else
 		return "Failed! Check your posted data.";
@@ -31,16 +33,16 @@ exports.handler = async function(event, context) {
 
 function generateSignature(sdkKey, sdkSecret, meetingNumber, role) {
 
-	const iat = Math.round(new Date().getTime() / 1000) - 30;
-  	const exp = iat + 60 * 60 * 2;
-	const oHeader = { alg: 'HS256', typ: 'JWT' }
+	const iat		= Math.round(new Date().getTime() / 1000) - 30;
+  	const exp		= iat + 60 * 60 * 2;
+	const oHeader	= { alg: 'HS256', typ: 'JWT' }
   
 	const oPayload = {
-	  sdkKey: sdkKey,
-	  mn: meetingNumber,
-	  role: role,
-	  iat: iat,
-	  exp: exp,
+	  sdkKey:	sdkKey,
+	  mn:		meetingNumber,
+	  role:		role,
+	  iat:		iat,
+	  exp:		exp
 	}
   
 	const sHeader = JSON.stringify(oHeader)
